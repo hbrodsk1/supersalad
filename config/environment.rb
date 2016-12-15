60310d3bb556fd8b5733e6de97bca62980f9d588
Harry Brodsky <hbrodsk1@binghamton.edu>
2016-12-15 01:22:02 -0500

    Mailer try 2

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-   :tls => true,
-   :address => "smtp.gmail.com",
-   :port => "587",
-   :domain => "gmail.com",
-   :authentication => :login,
-   :user_name => "[username]",
-   :password => "[password]"
- }
+  :user_name => ENV["SENDGRID_USERNAME"],
+  :password => ENV["SENDGRID_PASSWORD"],
+  :domain => 'supersalad.herokuapp.com/',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
