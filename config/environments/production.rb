ceaee72207c13d8ec3308f48064d6d6039878609
Harry Brodsky <hbrodsk1@binghamton.edu>
2016-12-14 20:49:32 -0500

    Try fixing Oauth and mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  # Default Mailer Host
+  config.action_mailer.default_url_options = { :host => 'https://supersalad.herokuapp.com/' }
+  config.action_mailer.delivery_method = :smtp
   # Code is not reloaded between requests.
   config.cache_classes = true
 
