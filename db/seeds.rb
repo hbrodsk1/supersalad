51ac4b7a5deae2d8522cf4259ab250c7f9c82023
Harry Brodsky <hbrodsk1@binghamton.edu>
2016-12-20 13:22:48 -0500

    Add seed info

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 10.times { FactoryGirl.create(:user) }
+
+soup = Food.create(kind: 'soup', title: 'default_soup', image: File.open(Rails.root + "app/assets/images/browse_soup.jpeg"))
+salad = Food.create(kind: 'salad', title: 'default_salad', image: File.open(Rails.root + "app/assets/images/browse_salad.jpeg"))
