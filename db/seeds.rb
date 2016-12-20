450288e75189d2fc060a0fddefe842b7871ec531
Harry Brodsky <hbrodsk1@binghamton.edu>
2016-12-20 13:33:20 -0500

    Seed data 4

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,4 @@
 #10.times { FactoryGirl.create(:user) }
 
 soup = Food.create(kind: 'soup', title: 'default_soup', image: Rails.root.join("app/assets/images/browse_soup.jpeg").open)
-salad = Food.create(kind: 'salad', title: 'default_salad', image: Rails.root.join("app/assets/images/browse_salad.jpeg").open))
+salad = Food.create(kind: 'salad', title: 'default_salad', image: Rails.root.join("app/assets/images/browse_salad.jpeg").open)
