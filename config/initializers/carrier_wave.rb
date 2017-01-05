a42fdbc0635f7727ac29e68c8006f60a63fe17de
Harry Brodsky <hbrodsk1@binghamton.edu>
2017-01-05 11:06:10 -0500

    Try AWS 2

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -8,6 +8,6 @@ if Rails.env.production?
 	    :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
 	    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
 		}
-		config.fog.directory = ENV['S3_BUCKET_NAME']
+		config.fog_directory = ENV['S3_BUCKET_NAME']
 	end
 end
